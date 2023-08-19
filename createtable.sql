8ea49f7e6f4f1d95f008f3e6515572a877d55cfc
ChigiriRyota <611chigiri@gmail.com>
2023-08-19 02:57:26 +0000

    StartHourとEndHourをint型に

--- createtable.sql
+++ createtable.sql
@@ -1,11 +1,12 @@
+-- 1つ目のグループ情報テーブル
 CREATE TABLE GroupInformation (
     GroupID VARCHAR(255) NOT NULL,
     GroupName VARCHAR(255) NOT NULL,
     Author VARCHAR(255) NOT NULL,
     StartDay VARCHAR(255) NOT NULL,
     EndDay VARCHAR(255) NOT NULL,
-    StartHour VARCHAR(255) NOT NULL,
-    EndHour VARCHAR(255) NOT NULL,
+    StartHour int NULL,
+    EndHour int NULL,
     PRIMARY KEY (GroupID)
 );
 
