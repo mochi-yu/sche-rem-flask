12868396a342102ed427e421ea94ba7c2d2a7b39
yuta mochizuki <mochi.2225.it@gmail.com>
2023-08-24 16:43:18 +0000

    feat: 入力を促すメールを送る処理を追加

--- src/logic/makeNewGroupLogic.py
+++ src/logic/makeNewGroupLogic.py
@@ -2,6 +2,7 @@ from models.create_group_request_param import CreateGroupRequestParam
 from models.group_info import GroupInfo
 from database.GroupInformationTable import insert_group
 from database.GroupUserTable import insert_group_users
+from sendEmailLogic import send_emails
 import uuid
 
 def make_new_group_logic(newGroup: CreateGroupRequestParam):
@@ -10,6 +11,15 @@ def make_new_group_logic(newGroup: CreateGroupRequestParam):
     createdGroup = insert_group(newGroup, groupID)
     addedUsers = insert_group_users(newGroup.groupUsers, groupID)
     # print(addedUsers)
+
+    # 参加者にメールを送る
+    mailTitle = "新しい日程調整が作成されました。"
+    mailBody = """
+新しい日程調整が作成されました。
+以下のリンクからあなたの日程調整を入力してください。
+URL：https://sche-rem.vercel.app/email/{groupID}
+    """
+    # send_emails(newGroup.groupUsers, mailTitle, mailBody)
 
     createdGroup["groupUsers"] = newGroup.groupUsers
     createdGroup["schedules"] = []
